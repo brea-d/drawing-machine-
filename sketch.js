4d272ca10b5e99d65adba990fc38512e2cc17929
brea-d <breanna.dulay@sjsu.edu>
2021-11-03 23:58:29 -0700

    3.6

---
+++ sketch.js
@@ -0,0 +1,42 @@
+let array = [];
+
+function setup() {
+  createCanvas(600, 600);
+  background(220);
+
+  strokeWeight(5);
+}
+
+function draw() {
+  if (mouseIsPressed) {
+    // stroke(map(mouseX, 0, 600, 0, 255, true));
+    // line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
+    // line(mouseX, mouseY, pmouseX, pmouseY);
+    
+    array.push([mouseX, mouseY]);
+  }
+}
+
+function keyTyped (){
+
+  console.log(`key ${key} is being typed`)
+
+
+  if (key === 's'){
+      // save this image 
+    saveCanvas('filename', 'png');
+  } else if (key === 'd'){
+        // display this image
+    beginShape();
+    for(let i = 0; i < array.length - 1; i++){
+      // line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
+    
+      // curve vertex 
+      curveVertex(array[i][0], array[i][1]);
+    }
+    endShape();
+
+  }
+
+  return false;
+}
